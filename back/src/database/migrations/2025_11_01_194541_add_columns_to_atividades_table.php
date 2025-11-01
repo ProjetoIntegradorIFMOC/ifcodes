024fd9d0e5ccb2adbce61f3f462286931fcd3a0c
Rafael José <rjbc@aluno.ifnmg.edu.br>
2025-11-01 17:51:02 -0300

    Migração para adicionar chave estrangeira referenciando a turma a qual a questão faz parte

---
+++ back/src/database/migrations/2025_11_01_194541_add_columns_to_atividades_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('atividade', function (Blueprint $table) {
+            $table->foreignId('turma_id')->constrained('turma')->onDelete('cascade')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('atividade', function (Blueprint $table) {
+            $table->dropForeign(['turma_id']);
+        });
+    }
+};
