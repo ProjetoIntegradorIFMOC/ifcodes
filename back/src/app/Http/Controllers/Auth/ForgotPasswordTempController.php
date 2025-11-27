d795f683c40f2aa83028a5e14c6162934910223c
Tefo02 <stefanolopes84@gmail.com>
2025-11-27 00:13:56 -0300

    refactor: remove unused Hash facade import in ForgotPasswordTempController

--- back/src/app/Http/Controllers/Auth/ForgotPasswordTempController.php
+++ back/src/app/Http/Controllers/Auth/ForgotPasswordTempController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SendTempPassword;
 
